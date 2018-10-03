af30fbbfc197843741c9400532354d9f9cbfbf14
RushaliSetia <43431650+RushaliSetia@users.noreply.github.com>
2018-10-03 18:17:45 +0530

    Add files via upload

---
+++ GrabAJob/src/question2/JobApplication.java
@@ -0,0 +1,60 @@
+package question2;
+
+public class JobApplication {
+
+	String userName;
+	int yearsOfExperience;
+	int currentSalary;
+	int expectedSalary;
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public int getYearsOfExperience() {
+		return yearsOfExperience;
+	}
+
+	public void setYearsOfExperience(int yearsOfExperience) {
+		this.yearsOfExperience = yearsOfExperience;
+	}
+
+	public int getCurrentSalary() {
+		return currentSalary;
+	}
+
+	public void setCurrentSalary(int currentSalary) {
+		this.currentSalary = currentSalary;
+	}
+
+	public int getExpectedSalary() {
+		return expectedSalary;
+	}
+
+	public void setExpectedSalary(int expectedSalary) {
+		this.expectedSalary = expectedSalary;
+	}
+
+	public JobApplication(String userName, int yearsOfExperience, int currentSalary, int expectedSalary) {
+		super();
+		this.userName = userName;
+		this.yearsOfExperience = yearsOfExperience;
+		this.currentSalary = currentSalary;
+		this.expectedSalary = expectedSalary;
+	}
+
+	public JobApplication() {
+		super();
+	}
+
+	@Override
+	public String toString() {
+		return "JobApplication [userName=" + userName + ", yearsOfExperience=" + yearsOfExperience + ", currentSalary="
+				+ currentSalary + ", expectedSalary=" + expectedSalary + "]";
+	}
+
+}
